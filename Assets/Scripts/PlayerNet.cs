3bd0f5117dd6ec74f2e1fdf71522e7553d127e3e
ahmadgalea <ahmadgalea@gmail.com>
2021-11-24 12:15:43 +1100

    Update PlayerNet.cs
    Enabled player click to swing net

--- Assets/Scripts/PlayerNet.cs
+++ Assets/Scripts/PlayerNet.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class PlayerNet : MonoBehaviour
 {
-   
+    private Animator animator = null;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        animator = GetComponent<Animator>();
+    }   
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("SwingNet",true);
+        }
+    }
+
+    void StopSwing()
+    {
+        animator.SetBool("SwingNet", false);
     }
 
 
