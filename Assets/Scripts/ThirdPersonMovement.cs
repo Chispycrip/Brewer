1c88c5309d5d35339930ba954629ed6193acac73
ahmadgalea <ahmadgalea@gmail.com>
2021-11-24 11:47:44 +1100

    Changed player controller to 'SimpleMove'

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -38,7 +38,7 @@ public class ThirdPersonMovement : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            controller.SimpleMove(moveDir.normalized * speed);
         }
     }
 
