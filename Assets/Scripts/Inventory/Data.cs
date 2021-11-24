79b27502fa4085a637efca071b56fa96a34e9c3c
stephensondylan <94809901+stephensondylan@users.noreply.github.com>
2021-11-24 10:35:43 +1030

    Changes to data files
    CritterData and PotionData now inherit from Data

---
+++ Assets/Scripts/Inventory/Data.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Data : ScriptableObject
+{
+    public string typeName; //name of the potion type
+    public int tier; //tier 1-4, golden is 4
+    public string trait; //speed, stealth or golden
+    public string description; //brief flavour text
+
+    public Image icon; //2D image for UI
+    public GameObject model; //3D model
+    public Material modelMaterial; //material used on the model
+}
