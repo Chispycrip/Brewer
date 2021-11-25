ea7f3543be524ed1ad82155b29cd2de583355b08
stephensondylan <94809901+stephensondylan@users.noreply.github.com>
2021-11-25 11:20:44 +1030

    InventoryUI implemented
    Not yet tested

--- Assets/Scripts/Inventory/InventoryUI.cs
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Inventory inventory; //the attached inventory
+    public ItemUI itemPrefab; //the prefab for ItemUI
+    public Jar jarPrefab; //the prefab for Jar
+    private Jar[] jars; //array of jars
+    
+    //Start is called before the first frame update
     void Start()
     {
-        
-    }
+        //create an array of jars the same size as the inventory
+        jars = new Jar[inventory.items.Length];
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //instantiate and initialise all jars in array and create an ItemUI for each jar
+        for (int i = 0; i < inventory.items.Length; i++)
+        {
+            jars[i] = Instantiate(jarPrefab, transform);
+            jars[i].itemUI = Instantiate(itemPrefab, jars[i].transform);
+            jars[i].itemUI.SetContents(inventory.items[i]);
+            jars[i].Init(this, i, jars[i].itemUI);
+        }
     }
 }
--- Assets/Scripts/Inventory/Jar.cs
+++ Assets/Scripts/Inventory/Jar.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Jar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ItemUI itemUI; //the ItemUI stored in this jar
+    public InventoryUI parentInv; //the inventoryUI that holds this jar
+    public int index; //the index position of this jar in the InventoryUI
+    public Draggable draggable; //the Draggable
+
+    //initialises the jar into it's inventory UI
+    public void Init(InventoryUI parent, int jarIndex, Draggable drag)
     {
-        
+        index = jarIndex;
+        parentInv = parent;
+        draggable = drag;
+        draggable.jar = this;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //updates the contents of this jar
+    public void UpdateItem(Data item)
     {
-        
+        //update the inventory array
+        parentInv.inventory.items[index] = item;
+
+        //update the itemUI to display the new data
+        itemUI.SetContents(item);
     }
 }
