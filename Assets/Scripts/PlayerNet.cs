172ea785b51e8fe6bdfe78a862b53ac7afa03aed
Billy502 <bilal_jalil@outlook.com>
2021-12-07 10:49:53 +1100

    Audio Mixer changes

--- Assets/Scripts/PlayerNet.cs
+++ Assets/Scripts/PlayerNet.cs
@@ -11,7 +11,7 @@ public class PlayerNet : MonoBehaviour
     private BoxCollider netCollider = null;
 
     bool swinging = false;
-  
+
 
     // Start is called before the first frame update
     void Start()
