71b93a4949d089293161266bbfe4e8c57b06db52
ahmadgalea <ahmadgalea@gmail.com>
2021-12-06 11:50:39 +1100

    Added prototype force trigger

---
+++ Assets/Scripts/PlayerForcer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerForcer : MonoBehaviour
+{
+    public Vector3 initialVelocity;
+
+
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    public void ApplyForce()
+    {
+        velocity = initialVelocity;
+    }
+}
